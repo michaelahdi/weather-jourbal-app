50ae5a93724de514f3f3faf3c5b152a8fa97ee86
michael-ahdi-aziz <michaelahdi6@gmail.com>
2021-08-24 21:44:15 +0300

    apikey setup

--- website/app.js
+++ website/app.js
@@ -3,3 +3,5 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+// setup apikey to link to openweathermap web 
+const apiKey = "9424b92c045f11c54f9de89f9147d73d" ;
