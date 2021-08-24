a9eb43ac7d86f01836027bbba2eecd4741fba182
michael-ahdi-aziz <michaelahdi6@gmail.com>
2021-08-24 21:25:23 +0300

    install dependencies and use them by express

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
