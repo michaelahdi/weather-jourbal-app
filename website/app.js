1c70637852bf20a7adce2e3480a7d13681361cf3
michael-ahdi-aziz <michaelahdi6@gmail.com>
2021-08-24 21:58:52 +0300

    set url to retrieve information

--- website/app.js
+++ website/app.js
@@ -2,6 +2,8 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 // setup apikey to link to openweathermap web 
-const apiKey = "9424b92c045f11c54f9de89f9147d73d" ;
+const apiKey = "&appid=9424b92c045f11c54f9de89f9147d73d&units=imperial" ;
+//setting url to get the information from openweather map
+const baseURL = `https://api.openweathermap.org/data/2.5/weather?zip=`
